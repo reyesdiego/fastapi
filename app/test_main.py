b8f8db29bc30a440ff8fecb094e3a00f9de86cff
diego <reyesdiego@hotmail.com>
2026-03-17 18:36:21 -0300

    unit test

---
+++ app/test_main.py
@@ -0,0 +1,10 @@
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World", 'status': 'ok', 'version': 'unknown'}
